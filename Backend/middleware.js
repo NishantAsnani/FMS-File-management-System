f0941dfa7bcc564705697f3432fe00ecde4349aa
NishantAsnani <asnaninishant2@gmail.com>
2024-04-05 12:48:51 +0530

    "Changed Middleware to Support Deployment"

--- Backend/middleware.js
+++ Backend/middleware.js
@@ -3,25 +3,31 @@ const User = require("./models/userSchema");
 
 const auth = async (req, res, next) => {
   try {
-    const token =req.headers.authorization;
-    const decoded = jwt.verify(token,process.env.SECRET_KEY);
-    if (!decoded.id) {
-      throw new Error();
+    const token = req.headers.authorization;
+    if (!token) {
+      throw new Error("Authorization token not provided");
     }
+
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    if (!decoded.id) {
+      throw new Error("Invalid token");
+    }
+
     const user = await User.findOne({
       _id: decoded.id,
       "tokens.token": token,
     });
 
     if (!user) {
-      throw new Error();
+      throw new Error("User not found");
     }
+
     req.token = token;
     req.user = user;
     next();
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(401)
+  } catch (error) {
+    console.error("Authentication error:", error.message);
+    res.sendStatus(401);
   }
 };
 
